13a40849475290fd9bb1b57c57831392f9777920
sean <anonymous@anonymous.com>
2017-12-10 22:02:24 -0800

    Update blocking rules

--- zhihuFilter.js
+++ zhihuFilter.js
@@ -1,6 +1,9 @@
 var patternsToBeRemoved = [
 	// Delete LIVE ads in the right column
-	"//div[contains(@class, 'RelatedLives-title')]/parent::div"
+	"//div[contains(@class, 'RelatedLives-title')]/parent::div",
+	// Delete right column ads
+	"//div[@data-za-module='ExternalAdItem']",
+	"//div[@data-za-detail-view-path-module='ExternalAdItem']"
 ];
 // All patterns, in this category, should be matching div[@class='Card TopstoryItem']
 var patternsToBeHidden = [
@@ -8,12 +11,8 @@ var patternsToBeHidden = [
 	"//button[contains(@class, 'TopstoryItem-advertButton')]/parent::div/parent::div",
 	// Hide LIVE participation posts
 	"//span[contains(text(), '参与了 Live')]/parent::div/parent::div/parent::div/parent::div[@class='Card TopstoryItem']",
-	// Hide LIVE promotion posts
-	"//span[contains(text(), '对 Live 感兴趣')]/parent::div/parent::div/parent::div/parent::div[@class='Card TopstoryItem']",
-	// Hide posts from certified org accounts on timeline
+	// Hide posts from certified org accounts
 	"//a[contains(@data-tooltip, '已认证的官方帐号')]/parent::span/parent::div/parent::div/parent::div/parent::div/parent::div[@class='Card TopstoryItem']",
-	// Hide posts from certified org accounts on question
-	"//a[contains(@data-tooltip, '已认证的官方帐号')]/parent::span/parent::div/parent::div/parent::div/parent::div/parent::div/parent::div/parent::div[@class='List-item']",
 	// Hide LIVE transaction posts
 	"//span[contains(text(), '赞助了 Live')]/parent::div/parent::div/parent::div/parent::div",
 	// Hide EBook Ads cards
